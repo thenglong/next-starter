13d8c31c54cbb3c19a5b8d6c9c0454e390d2fc82
thenglong <thenglongheng2k@gmail.com>
2021-06-26 11:55:56 +0700

    modified:   .github/workflows/node.js.yml 	modified:   next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  env: {
+    NEXT_PUBLIC_BASE_PATH: "/next-starter",
+  },
   reactStrictMode: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
